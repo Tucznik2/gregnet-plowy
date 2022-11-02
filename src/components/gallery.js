ee9ea2732477687def3ee7b8277dce8fe4805c34
Tucznik2 <f3kula@gmail.com>
2022-11-02 19:32:40 +0100

    switching working on img piecie

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -8,34 +8,36 @@ import Photo5 from '../images/realizations/05.jpg';
 export default function Gallery() {
   const [fadeProp, setFadeProp] = useState({ fade: 'fade-in' });
   const [imgIndex, setCurrentIndex] = useState(0);
-  const images = [Photo1, Photo2, Photo3, Photo4, Photo5];
+  const images = [Photo1, Photo3, Photo2, Photo4, Photo5];
 
   useEffect(() => {
     const fadeTimeout = setInterval(() => {
-      fadeProp.fade === 'fade-in'
-        ? setFadeProp({ fade: 'fade-out' })
-        : setFadeProp({ fade: 'fade-in' });
-    }, 2000);
+      if (fadeProp.fade === 'fade-in') {
+        setFadeProp({
+          fade: 'fade-out',
+        });
+      } else {
+        setFadeProp({
+          fade: 'fade-in',
+        });
+      }
+    }, 1750);
 
     return () => clearInterval(fadeTimeout);
   }, [fadeProp]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (imgIndex === images.length - 1) {
-        setCurrentIndex(0);
-      } else {
-        setCurrentIndex(imgIndex + 1);
-      }
-    }, 2000);
+      setCurrentIndex((prev) => (prev + 1) % images.length);
+    }, 3500);
 
     return () => clearInterval(intervalId);
-  });
+  }, []);
 
   return (
     <div className="about-images w-full grid">
-      <div className="about-img col-span-2 max-h-96">
-        <img className={fadeProp} src={images[imgIndex]} alt="realization-1" />
+      <div className={`about-img col-span-2 ${fadeProp.fade}`}>
+        <img src={images[imgIndex]} alt="realization-1" />
       </div>
       <div className="about-img sm:col-span-2 sm:row-start-2 sm:col-start-3">
         <img src={Photo2} alt="realization-2" />
